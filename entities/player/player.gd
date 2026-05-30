6bac7d2be472320103a75c52d06a14b172f4f531
Maskaradof <leonidbelozercev558@gmail.com>
2026-05-30 19:01:41 +0200

    Added run on left shift

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED # Прячем курсор при старте игры
 
@@ -27,13 +27,17 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	var SPEED = 5.0
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if Input.is_action_pressed("ui_run"):
+		SPEED = 10.0
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
 
 	move_and_slide()
